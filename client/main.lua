4d2b2b55c85fcd902806ab5e0c6ca93d6281756c
Deltanic <rdg94@live.nl>
2018-03-25 22:54:12 +0200

    Fix sync issues after client session disconnects.

--- client/main.lua
+++ client/main.lua
@@ -62,7 +62,30 @@ end)
 
 RegisterNetEvent('nitro:__update')
 AddEventHandler('nitro:__update', function (playerServerId, boostEnabled, purgeEnabled, lastVehicle)
-  local player = GetPlayerPed(GetPlayerFromServerId(playerServerId))
+  local playerId = GetPlayerFromServerId(playerServerId)
+
+  -- Sometimes, the source player is disconnected from our session. If we don't
+  -- check for that, their player ID will be -1. GetPlayerPed(-1) is our local
+  -- player, so the logic to apply nitro sync will apply it to our vehicle when
+  -- that happens.
+  --
+  -- Say, the source player enables nitro, but is not connected in our session.
+  -- Nitro is then synced on the vehicle for player -1, which is us, so nitro is
+  -- activated on our vehicle. However, because we're not actually pressing the
+  -- nitro key, our client will update the nitro state accordingly, and turn it
+  -- off. That then syncs to the original source player, who has the exact same
+  -- network issue as we do. Nitro will be disabled on his vehicle, but he's
+  -- still pressing the nitro key, so it's being enabled right after. Long story
+  -- short, this causes an infinite sync loop between all clients as long as at
+  -- least one player has nitro activated.
+  --
+  -- Therefor, simply check if the source player is connected to our session. If
+  -- not, ignore the synced state and don't do anything.
+  if not NetworkIsPlayerConnected(playerId) then
+    return
+  end
+
+  local player = GetPlayerPed(playerId)
   local vehicle = GetVehiclePedIsIn(player, lastVehicle)
   local driver = GetPedInVehicleSeat(vehicle, -1)
 
