93c9434685c236927742cfd3a042b232aa1b7fff
Xinerki <simasadves@gmail.com>
2020-06-17 19:46:10 +0300

    add fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,13 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'bodacious'
+game 'gta5'
+
+name 'shockwave-nitro'
+description 'Nitro System'
+author 'Deltanic - https://github.com/Deltanic/'
+url 'https://github.com/swcfx/sw-nitro'
 
 client_script 'client/utils.lua'
+-- client_script 'client/fuel.lua'
 client_script 'client/ptfx.lua'
 client_script 'client/boost.lua'
 client_script 'client/purge.lua'
