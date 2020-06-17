897578cf9d3463da7da9876f21a63f558f7d4b79
Deltanic <rdg94@live.nl>
2020-06-17 23:16:41 +0200

    Fix IsVehicleElectric.

--- client/utils.lua
+++ client/utils.lua
@@ -1,5 +1,22 @@
+local ELECTRIC_VEHICLES = {
+  [GetHashKey('AIRTUG')] = true,
+  [GetHashKey('CYCLONE')] = true,
+  [GetHashKey('CADDY')] = true,
+  [GetHashKey('CADDY2')] = true,
+  [GetHashKey('CADDY3')] = true,
+  [GetHashKey('DILETTANTE')] = true,
+  [GetHashKey('IMORGON')] = true,
+  [GetHashKey('KHAMEL')] = true,
+  [GetHashKey('NEON')] = true,
+  [GetHashKey('RAIDEN')] = true,
+  [GetHashKey('SURGE')] = true,
+  [GetHashKey('VOLTIC')] = true,
+  [GetHashKey('TEZERACT')] = true
+}
 
+-- TODO: Replace with `FLAG_IS_ELECTRIC` from vehicles.meta:
+-- https://gtamods.com/wiki/Vehicles.meta
 function IsVehicleElectric(vehicle)
-  local maxFuelLevel = GetVehicleHandlingFloat(vehicle, 'CHandlingData', 'fPetrolTankVolume')
-  return not (maxFuelLevel > 0.0)
+  local model = GetEntityModel(vehicle)
+  return ELECTRIC_VEHICLES[model] or false
 end
